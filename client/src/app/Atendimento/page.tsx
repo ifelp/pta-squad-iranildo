b71e38bf2724814f958afd3460ed875097d09652
ifelp <iranildofpersonal18@gmail.com>
2025-05-30 07:55:29 -0300

    refatorando tela de agendamentos

--- client/src/app/Atendimento/page.tsx
+++ client/src/app/Atendimento/page.tsx
@@ -59,52 +59,54 @@ const AtendimentoPage = () => {
 });
 
   return (
-    <div className="p-6 space-y-4 text-sm">
-      {/* Título */}
-      <h1 className="text-3xl font-bold text-gray-800">Atendimento</h1>
+    <div className="pt-6 px-32 space-y-4 text-sm gap-8">
+      <div className="flex flex-col w-full h-[180px] gap-2">
+        {/* Título */}
+        <h1 className="text-5xl font-bold text-gray-800">Atendimento</h1>
 
-      {/* Filtros */}
-      <Filters
-        onSearch={setSearch}
-      />
+        {/* Filtros */}
+        <Filters
+          onSearch={setSearch}
+          />
+      </div>
 
-      <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-3 h-[40px]">
         {/* Tabs Histórico/Agendamento */}
-        <div className="flex gap-2">
+        <div className="flex gap-2 bg-[#F0F0F0] justify-center rounded-[12px] w-[243px] h-[48px]">
             <button
-            className={`px-4 py-2 rounded-full border text-sm ${tab === 'historico' ? 'bg-gray-300 font-bold' : ''}`}
+            className={`px-4 py-2 rounded-[12px] text-sm ${tab === 'historico' ? 'bg-white font-bold' : ''}`}
             onClick={() => setTab('historico')}
             >
-            Histórico
+            Agendamento
             </button>
             <button
-            className={`px-4 py-2 rounded-full border text-sm ${tab === 'agendamento' ? 'bg-gray-300 font-bold' : ''}`}
+            className={`px-4 py-2 rounded-[12px] text-sm ${tab === 'agendamento' ? 'bg-white font-bold' : ''}`}
             onClick={() => setTab('agendamento')}
             >
-            Agendamento
+            Histórico
             </button>
         </div>
 
         {/* Inputs de data à direita */}
-        <div className="flex gap-2">
+        <div className="flex gap-2 h-full">
             <input
             type="date"
             value={dateStart}
             onChange={(e) => setDateStart(e.target.value)}
-            className="border px-3 py-1 rounded text-sm"
+            className="border px-3 py-1 rounded text-sm h-full"
             />
             <input
             type="date"
             value={dateEnd}
             onChange={(e) => setDateEnd(e.target.value)}
-            className="border px-3 py-1 rounded text-sm"
+            className="border px-3 py-1 rounded text-sm h-full"
             />
         </div>
         </div>
 
 
       {/* Grid de Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
         {atendimentosFiltrados.map((item, index) => (
           <Card key={index} {...item} />
         ))}
@@ -112,7 +114,7 @@ const AtendimentoPage = () => {
 
       {/* Botão Nova Consulta */}
       <div className="flex justify-end">
-        <button className="bg-green-500 text-white px-4 py-2 rounded-full text-sm hover:bg-green-600 transition">
+        <button className="bg-green-500 w-[205px] h-[48px] text-white px-4 py-2 rounded-full text-sm hover:bg-green-600 transition">
           Nova Consulta
         </button>
       </div>
--- client/src/components/Filters/filters.tsx
+++ client/src/components/Filters/filters.tsx
@@ -6,17 +6,17 @@ interface FiltersProps {
 
 const Filters: React.FC<FiltersProps> = ({ onSearch }) => {
   return (
-    <div className="flex flex-col gap-2 text-sm w-full">
+    <div className="flex flex-col gap-2 text-sm w-[620px] h-full">
       <label className="text-gray-700 font-semibold">Quem é o médico?</label>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center w-full">
         <input
           type="text"
           placeholder="Pesquise aqui..."
           onChange={(e) => onSearch(e.target.value)}
-          className="border px-3 py-1 rounded w-full md:w-64 text-sm"
+          className="border px-3 py-1 rounded w-full h-[48px] text-sm"
         />
         <button
-          className="bg-[#7D1AD7] text-white px-4 py-1.5 rounded-full text-sm hover:bg-purple-800 transition"
+          className="bg-[#7D1AD7] text-white h-full px-4 py-1.5 rounded-full text-sm hover:bg-purple-800 transition"
         >
           Buscar
         </button>
