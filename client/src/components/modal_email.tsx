d2de4c2681b3f8e71b72ef3862348de0cfffd0d1
ifelp <iranildofpersonal18@gmail.com>
2025-05-29 22:45:03 -0300

    refactor(email): adding react-hook-form to email modal.

--- client/src/components/modal_email.tsx
+++ client/src/components/modal_email.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import logoCiti from "../assets/logoCiti.png";
 import closeIcon from "../assets/close.png";
+import {useForm, SubmitHandler} from 'react-hook-form'
 
 const positionStyle: React.CSSProperties = {
     display: "flex",
@@ -30,7 +31,17 @@ const inputStyle: React.CSSProperties = {
     height: "50px"
 }
 
+interface Email{
+    email:string
+}
+
 export default function SendEmail(){
+    const {register, formState: {errors}, handleSubmit} = useForm<Email>()
+
+    const sendEMail: SubmitHandler<Email> = (data)=>{
+        console.log('Email enviado.')
+        console.log(data)
+    }
 return (
     <div style={{...positionStyle, width: "420px", height: "423px", borderRadius: "24px", padding: "48px"}}>
        
@@ -55,7 +66,8 @@ return (
 
         <div style={{display: "flex", flexDirection: "column", gap: "12px"}}>
             <label htmlFor='emailTutor' style={{fontWeight: "bold"}}>Email</label>
-            <input type='email' id='emailTutor' name='email_tutor' placeholder=' Digite aqui...' style={inputStyle}/>
+            <input type='email' id='emailTutor' placeholder=' Digite aqui...' style={inputStyle} 
+            {...register('email', {required: 'Email do tutor é obrigatorio.'})}/>
         </div>
 
         <div>
