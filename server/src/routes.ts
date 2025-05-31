4c0444bb1a441752f031447cd5aba35273fb5f0b
AlanyVitoria <alanyvitoria03@gmail.com>
2025-05-31 01:51:57 -0300

    Criação de controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import ConsultaController from "./controllers/ConsultaController";
 
 const routes = express.Router();
 
@@ -7,5 +8,10 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+routes.post("/consulta", ConsultaController.create);
+routes.get("/consulta", ConsultaController.get);
+routes.delete("/consulta/:id", ConsultaController.delete);
+routes.patch("/consulta/:id", ConsultaController.update);
+routes.get("/consulta/:id", ConsultaController.getById);
 
 export default routes;
