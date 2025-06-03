9d9233a34ecfc0a1e36f9a4c3a7a63b109b8fc84
ifelp <iranildofpersonal18@gmail.com>
2025-06-02 22:35:33 -0300

    adding cors to server page.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,16 @@ import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
 import "@database";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+}))
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
 
