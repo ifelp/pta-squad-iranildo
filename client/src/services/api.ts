527e62a23fcf9bd790e473210f960af5808503a0
ifelp <iranildofpersonal18@gmail.com>
2025-06-06 22:27:51 -0300

    adding deployed server integration

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://pta-squad-iranildo-back.onrender.com/',
     headers:{
         'Content-Type': 'application/json'
     }
